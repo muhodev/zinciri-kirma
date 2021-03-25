3855e0944f47ff4ed9e02517b407c88ba83ae597
muhodev <hi@muho.dev>
2021-03-25 15:33:23 +0300

    display weekday on calendar

--- web/pages/calendar/[yil]/[ay]/[gun]/index.js
+++ web/pages/calendar/[yil]/[ay]/[gun]/index.js
@@ -3,6 +3,37 @@ import { useDate } from "hooks";
 import style from "./style.module.css";
 import { useRouter } from "next/router";
 
+const weekDays = [
+  {
+    title: "Pazartesi",
+    short: "Pzt",
+  },
+  {
+    title: "Salı",
+    short: "Sal",
+  },
+  {
+    title: "Çarşamba",
+    short: "Çar",
+  },
+  {
+    title: "Perşembe",
+    short: "Per",
+  },
+  {
+    title: "Cuma",
+    short: "Cum",
+  },
+  {
+    title: "Cumartesi",
+    short: "Cmt",
+  },
+  {
+    title: "Pazar",
+    short: "Paz",
+  },
+];
+
 export default function HedefPage(props) {
   const { query } = useRouter();
   const { days } = useDate({ yil: query.yil, ay: query.ay, gun: query.gun });
@@ -10,11 +41,22 @@ export default function HedefPage(props) {
   return (
     <DefaultLayout>
       <div className={style.hedef}>
-        {days.map((item, index) => (
-          <div className="border-r border-b" key={index}>
-            <div className="text-sm text-center py-1">{item.value}</div>
-          </div>
-        ))}
+        <div className="w-full grid gap-0 grid-cols-7">
+          {weekDays.map((weekDay, index) => (
+            <div className="border-r text-center" key={index}>
+              <span className="text-gray-500 text-xs py-1">
+                {weekDay.short}
+              </span>
+            </div>
+          ))}
+        </div>
+        <div className="grid gap-0 grid-cols-7 flex-1">
+          {days.map((item, index) => (
+            <div className="border-r border-b" key={index}>
+              <div className="text-sm text-center py-1">{item.value}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </DefaultLayout>
   );
